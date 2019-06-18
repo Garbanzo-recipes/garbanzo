e14c5cbc1c039522cbca8078cedd37b8b65621e9
Daniel Lemper <daniel.lemper@fuchs.de>
2019-06-18 21:53:07 +0200

    feature: added persistancy to vuex store with localstorage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -66,4 +67,5 @@ export default new Vuex.Store({
   actions: {
 
   },
+  plugins: [createPersistedState()],
 });
