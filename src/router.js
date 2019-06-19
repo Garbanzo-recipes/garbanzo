746542d609d3f6a168043ff86bb3112455238834
Daniel Lemper <daniel.lemper@fuchs.de>
2019-06-19 11:11:28 +0200

    bugfix: reloading the page when not in home view resulted in 404, as the history mode of vue-router is not supported on gh-pages

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import About from './views/About.vue';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history', // not compatible with gh-pages
   base: process.env.BASE_URL,
   routes: [
     {
