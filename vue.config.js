3683b04c07336c9fe2b6128d65148c9fbb1a9d0c
Daniel Lemper <daniel.lemper@fuchs.de>
2019-06-27 09:14:31 +0200

    set appleMobileWebAppCapable again to yes as the camera problems depend on "add to homescreen" and not on this setting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/garbanzo/' : '/',
   pwa: {
-    appleMobileWebAppCapable: 'no', // explicitly disabled, as camera doesn't work on iOS in fullscreen mode
+    appleMobileWebAppCapable: 'yes',
   },
 };
