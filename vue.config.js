9378ec429e54da7fcb7fe12fdf495cf9a6089ede
Daniel Lemper <daniel.lemper@fuchs.de>
2019-11-11 18:55:14 +0100

    add purgecss to decrease css size at build time

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -11,4 +15,16 @@ module.exports = {
   pwa: {
     appleMobileWebAppCapable: 'yes',
   },
+  configureWebpack: {
+    // Merged into the final Webpack config
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ])
+      })
+    ]
+  }
 };
