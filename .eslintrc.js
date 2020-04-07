bd9fbb14e76aba6289a3c5a2dd4d578092696ea8
Daniel Lemper <ich@dlemper.de>
2020-04-07 21:17:47 +0200

    fix linting rule of current eslint regarding template strings; npm dedupe

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,13 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // Adding next two rules to avoid bug in babel-eslint:
+    // https://github.com/babel/babel-eslint/issues/799
+    indent: [
+      'warn', 2,
+      { ignoredNodes: ['TemplateLiteral'] }
+    ],
+    'template-curly-spacing': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
